e86ecc11c9c1b2dc18111cc5b5ca3b9f162f232a
Darymee <danieledaniele3005@gmail.com>
2023-07-30 09:44:02 +0200

    Update actions.ts

--- lib/actions.ts
+++ lib/actions.ts
@@ -13,7 +13,6 @@ import {
 import { ProjectForm } from "@/common.types";
 
 const isProduction = process.env.NODE_ENV === "production";
-
 const apiUrl = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
   : "http://127.0.0.1:4000/graphql";
@@ -23,7 +22,7 @@ const apiKey = isProduction
   : "letmein";
 
 const serverUrl = isProduction
-  ? process.env.NEXTAUTH_URL
+  ? process.env.NEXT_PUBLIC_SERVER_URL
   : "http://localhost:3000";
 
 const client = new GraphQLClient(apiUrl);
